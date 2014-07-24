5696d67b21df96e478c7e1a350edb71d0484fca4
unknown <maceo.marquez@NYCMRM-NWS4188.na.corp.ipgnetwork.com>
2014-07-24 15:29:57 -0400

    First add

---
+++ js/holidays.js
@@ -0,0 +1,139 @@
+{
+  "holidays": [
+    {
+      "Day": "Monday, May 26",
+      "Holiday": "Memorial Day*",
+      "Date": "26-May-2014"
+    },
+    {
+      "Day": "Thursday, May 29",
+      "Holiday": "Solemnity of the Ascension",
+      "Date": "29-May-2014"
+    },
+    {
+      "Day": "Wednesday, June 4",
+      "Holiday": "Shavuot",
+      "Date": "4-Jun-2014"
+    },
+    {
+      "Day": "Thursday, June 5",
+      "Holiday": "Shavuot",
+      "Date": "5-Jun-2014"
+    },
+    {
+      "Day": "Friday, July 4",
+      "Holiday": "Independence Day*",
+      "Date": "4-Jul-2014"
+    },
+    {
+      "Day": "Monday, July 28",
+      "Holiday": "Idul-Fitr",
+      "Date": "28-Jul-2014"
+    },
+    {
+      "Day": "Tuesday, July 29",
+      "Holiday": "Idul-Fitr",
+      "Date": "29-Jul-2014"
+    },
+    {
+      "Day": "Wednesday, July 30",
+      "Holiday": "Idul-Fitr",
+      "Date": "30-Jul-2014"
+    },
+    {
+      "Day": "Friday, Aug 15",
+      "Holiday": "Feast of the Assumption",
+      "Date": "15-Aug-2014"
+    },
+    {
+      "Day": "Monday, Sept 1",
+      "Holiday": "Labor Day*",
+      "Date": "1-Sep-2014"
+    },
+    {
+      "Day": "Thursday, Sept 25",
+      "Holiday": "Rosh Hashanah",
+      "Date": "25-Sep-2014"
+    },
+    {
+      "Day": "Friday, Sept 26",
+      "Holiday": "Rosh Hashanah",
+      "Date": "26-Sep-2014"
+    },
+    {
+      "Day": "Saturday, Oct 4",
+      "Holiday": "Idul-Adha and Yom Kippur",
+      "Date": "4-Oct-2014"
+    },
+    {
+      "Day": "Sunday, Oct 5",
+      "Holiday": "Idul-Adha",
+      "Date": "5-Oct-2014"
+    },
+    {
+      "Day": "Monday, Oct 6",
+      "Holiday": "Idul-Adha",
+      "Date": "6-Oct-2014"
+    },
+    {
+      "Day": "Thursday, Oct 9",
+      "Holiday": "Succoth",
+      "Date": "9-Oct-2014"
+    },
+    {
+      "Day": "Friday, Oct 10",
+      "Holiday": "Succoth",
+      "Date": "10-Oct-2014"
+    },
+    {
+      "Day": "Monday, Oct 13",
+      "Holiday": "Columbus Day",
+      "Date": "13-Oct-2014"
+    },
+    {
+      "Day": "Thursday, Oct 16",
+      "Holiday": "Shemini Atzereth",
+      "Date": "16-Oct-2014"
+    },
+    {
+      "Day": "Friday, Oct 17",
+      "Holiday": "Simchas Torah",
+      "Date": "17-Oct-2014"
+    },
+    {
+      "Day": "Thursday, Oct 23",
+      "Holiday": "Diwali",
+      "Date": "23-Oct-2014"
+    },
+    {
+      "Day": "Saturday, Nov 1",
+      "Holiday": "All Saints Day",
+      "Date": "1-Nov-2014"
+    },
+    {
+      "Day": "Tuesday, Nov 4",
+      "Holiday": "Election Day",
+      "Date": "4-Nov-2014"
+    },
+    {
+      "Day": "Tuesday, Nov 11",
+      "Holiday": "Veterans Day",
+      "Date": "11-Jul-2014"
+    },
+    {
+      "Day": "Thursday, Nov 27",
+      "Holiday": "Thanksgiving*",
+      "Date": "27-Nov-2014"
+    },
+    {
+      "Day": "Monday, Dec 8",
+      "Holiday": "Immaculate Conception",
+      "Date": "8-Dec-2014"
+    },
+    {
+      "Day": "Thursday, Dec 25",
+      "Holiday": "Christmas*",
+      "Date": "25-Dec-2014"
+    }
+  ]
+}
